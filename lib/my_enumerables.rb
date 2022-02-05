d800fcb510b530144b61ff57fec85b0d978cb1c0
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-02-05 09:41:07 +0700

    Assigment Completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,65 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    arr_length = self.length
+    arr_length.times { |index| yield self[index], index }
+
+    self
+  end
+
+  def my_select
+    result = []
+    self.length.times { |index| result << self[index] if yield(self[index]) }
+    
+    result
+  end
+
+  def my_all?
+    self.length.times { |index| return false unless yield(self[index]) }
+    
+    true
+  end
+
+  def my_any?
+    self.length.times { |index| return true if yield(self[index]) }
+    
+    false
+  end
+
+  def my_none?
+    self.length.times { |index| return false if yield(self[index]) }
+
+    true
+  end
+
+  def my_count
+    if block_given?
+      count = 0
+      
+      self.length.times { |index| count += 1 if yield(self[index]) }
+
+      count
+    else
+      self.length
+    end
+  end
+
+  def my_map
+    result = []
+
+    self.length.times { |index| result << yield(self[index]) }
+
+    result
+  end
+
+  def my_inject(init_value)
+
+    self.length.times do |index|
+      init_value = yield(init_value, self[index])
+    end
+    
+    init_value
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +68,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    arr_length = self.length
+    arr_length.times { |index| yield self[index] }
+
+    self
+  end
 end
